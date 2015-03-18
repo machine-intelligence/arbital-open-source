e150b46e60cfb431f885a5f662843a506a59f8bd
Alexei Andreev <alexei.andreev@gmail.com>
2015-03-18 10:35:25 -0700

    Not showing autosave alert. Showing fullname for tags now.

--- src/site/static/js/editPage.js
+++ src/site/static/js/editPage.js
@@ -221,13 +221,13 @@ $(function() {
 })
 window.setInterval(function(){
 	if (!canAutosave) return;
-	$("#autosave-label").text("Saving...").show();
+	//$("#autosave-label").text("Saving...").show();
 	callPageHandler(true, false, function(r) {
 		if (r === undefined) {
 			$("#autosave-label").hide();
 		} else {
 			$("body").attr("privacy-key", r);
-			$("#autosave-label").text("Saved!").show();
+			//$("#autosave-label").text("Saved!").show();
 		}
 	});
 }, 5000);
