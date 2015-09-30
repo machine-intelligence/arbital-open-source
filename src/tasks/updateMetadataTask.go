07f03fbe7c75a24571219d318b70c2b57af789f9
Robert Lecnik <robertlecnik@gmail.com>
2015-09-29 20:36:40 -0700

    updateMetadataTask.go now updates the pageInfos table

--- src/tasks/updateMetadataTask.go
+++ src/tasks/updateMetadataTask.go
@@ -33,13 +33,24 @@ func (task *UpdateMetadataTask) Execute(c sessions.Context) (delay int, err erro
 	c.Debugf("==== UPDATE METADATA START ====")
 	defer c.Debugf("==== UPDATE METADATA COMPLETED ====")
 
-	// Compute all priors.
+	// Regenerate pages and links tables
 	err = database.QuerySql(c, `
 		SELECT pageId,edit,text
 		FROM pages
 		WHERE isCurrentEdit`, updateMetadata)
 	if err != nil {
-		c.Debugf("ERROR: %v", err)
+		c.Debugf("ERROR, failed to update pages and pageLinks: %v", err)
+		return -1, err
+	}
+
+	// Regenerate pageInfos table
+	err = database.QuerySql(c, `
+		SELECT pageId, MAX(if(isCurrentEdit, edit, 0)), MAX(edit), MIN(createdAt)
+		FROM pages
+		WHERE 1
+		GROUP BY pageId`, updatePageInfos)
+	if err != nil {
+		c.Debugf("ERROR, failed to update pageInfos: %v", err)
 		return -1, err
 	}
 	return 0, err
@@ -75,6 +86,41 @@ func updateMetadata(c sessions.Context, rows *sql.Rows) error {
 	if err != nil {
 		tx.Rollback()
 		return fmt.Errorf("Couldn't update links: %v", err)
+	}
+
+	// Commit transaction.
+	err = tx.Commit()
+	if err != nil {
+		tx.Rollback()
+		return err
+	}
+
+	return nil
+}
+
+func updatePageInfos(c sessions.Context, rows *sql.Rows) error {
+	var pageId, currentEdit, maxEdit int64
+	var createdAt string
+	if err := rows.Scan(&pageId, &currentEdit, &maxEdit, &createdAt); err != nil {
+		return fmt.Errorf("failed to scan a page: %v", err)
+	}
+
+	// Begin the transaction.
+	tx, err := database.NewTransaction(c)
+	if err != nil {
+		return err
+	}
+
+	// Update pageInfos summary
+	hashmap := make(map[string]interface{})
+	hashmap["pageId"] = pageId
+	hashmap["currentEdit"] = currentEdit
+	hashmap["maxEdit"] = maxEdit
+	hashmap["createdAt"] = createdAt
+	query := database.GetInsertSql("pageInfos", hashmap, "currentEdit", "maxEdit", "createdAt")
+	if _, err := tx.Exec(query); err != nil {
+		tx.Rollback()
+		return fmt.Errorf("Couldn't update pageInfos table: %v", err)
 	}
 
 	// Commit transaction.
