f7b299a72f47c68e00a876dc7859f1578e12d0e2
Alexei Andreev <alexei.andreev@gmail.com>
2015-10-25 16:58:22 -0700

    Just using the same hardcoded pages locally and live for the index page

--- src/site/indexPage.go
+++ src/site/indexPage.go
@@ -4,7 +4,6 @@ package site
 import (
 	"zanaduu3/src/core"
 	"zanaduu3/src/pages"
-	"zanaduu3/src/sessions"
 )
 
 type featuredDomain struct {
@@ -38,61 +37,35 @@ func indexRenderer(params *pages.HandlerParams) *pages.Result {
 	data.User = u
 
 	// Manually load some pages we like
-	if sessions.Live {
-		data.FeaturedDomains = append(data.FeaturedDomains,
-			&featuredDomain{
-				DomainAlias: "vat",
-				DomainName:  "Value Alignment Theory",
-				ImageUrl:    "/static/images/vatIndexLogo.png",
-				ChildIds: []int64{
-					8639103000879599414, // VAT
-					4213693741839491939, // List: value alignment subjects
-					7722661858289734773, // Value alignment problem
-					3158562585659930031, // Orthogonality theses
-					6820582940749120623, // Ontology identification problem
-					7879626441094927809, // Real world agents should be omni-safe
-					5534008569097047764, // Mindcrime
-					6053065048861201341, // Diamond maximizer
-				},
-			}, &featuredDomain{
-				DomainAlias: "arbital",
-				DomainName:  "Arbital",
-				ImageUrl:    "/static/images/arbitalIndexLogo.png",
-				ChildIds: []int64{
-					3560540392275264633, // What is Arbital
-					8992241719442104138, // Page hierarchy
-					7894360080081727761, // Arbital wiki page
-					5933317145970853046, // Editing
-					4675907493088898985, // Liking
-					8676677094741262267, // Voting
-				},
+	data.FeaturedDomains = append(data.FeaturedDomains,
+		&featuredDomain{
+			DomainAlias: "vat",
+			DomainName:  "Value Alignment Theory",
+			ImageUrl:    "/static/images/vatIndexLogo.png",
+			ChildIds: []int64{
+				8639103000879599414, // VAT
+				4213693741839491939, // List: value alignment subjects
+				7722661858289734773, // Value alignment problem
+				3158562585659930031, // Orthogonality theses
+				6820582940749120623, // Ontology identification problem
+				7879626441094927809, // Real world agents should be omni-safe
+				5534008569097047764, // Mindcrime
+				6053065048861201341, // Diamond maximizer
 			},
-		)
-	} else {
-		data.FeaturedDomains = append(data.FeaturedDomains,
-			&featuredDomain{
-				DomainAlias: "dom",
-				DomainName:  "Domain",
-				ImageUrl:    "/static/images/vatIndexLogo.png",
-				ChildIds: []int64{
-					8587544546020587460,
-					8329543642006081477,
-					8719043452159528073,
-					6507930240456827790,
-				},
-			}, &featuredDomain{
-				DomainAlias: "dom",
-				DomainName:  "Domain",
-				ImageUrl:    "/static/images/arbitalIndexLogo.png",
-				ChildIds: []int64{
-					8587544546020587460,
-					8329543642006081477,
-					8719043452159528073,
-					6507930240456827790,
-				},
+		}, &featuredDomain{
+			DomainAlias: "arbital",
+			DomainName:  "Arbital",
+			ImageUrl:    "/static/images/arbitalIndexLogo.png",
+			ChildIds: []int64{
+				3560540392275264633, // What is Arbital
+				8992241719442104138, // Page hierarchy
+				7894360080081727761, // Arbital wiki page
+				5933317145970853046, // Editing
+				4675907493088898985, // Liking
+				8676677094741262267, // Voting
 			},
-		)
-	}
+		},
+	)
 	data.PageMap = make(map[int64]*core.Page)
 	for _, domain := range data.FeaturedDomains {
 		for _, pageId := range domain.ChildIds {
