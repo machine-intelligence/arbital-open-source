e138d776ddf4566e5f8e8c55f33903f1e730acc2
Alexei Andreev <alexei.andreev@gmail.com>
2016-01-02 12:32:32 -0800

    Updated color scheme

--- src/tasks/sendOneEmailTask.go
+++ src/tasks/sendOneEmailTask.go
@@ -82,9 +82,9 @@ func (task *SendOneEmailTask) Execute(db *database.DB) (delay int, err error) {
 		// Create mail message
 		subject := fmt.Sprintf("%d new updates on Arbital", emailData.UpdateCount)
 		msg := &mail.Message{
-			Sender: "alexei@arbital.com",
-			To:     []string{emailData.UpdateEmailAddress},
-			//Bcc:      []string{"alexei@arbital.com"},
+			Sender:   "alexei@arbital.com",
+			To:       []string{emailData.UpdateEmailAddress},
+			Bcc:      []string{"alexei@arbital.com"},
 			Subject:  subject,
 			HTMLBody: emailData.UpdateEmailText,
 		}
