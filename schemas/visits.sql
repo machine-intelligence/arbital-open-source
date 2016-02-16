58b0d1fa7635e577ae7d8f7bce1c7f1cfae6f9fc
Alexei Andreev <alexei.andreev@gmail.com>
2016-02-15 21:40:00 -0800

    Critical fixes for the new URLs

--- schemas/visits.sql
+++ schemas/visits.sql
@@ -1,7 +1,7 @@
 /* Each row is a page-user pair with the date and time
 when the user has last seen the page. */
 CREATE TABLE visits (
-	/* User id. FK into users. */
+	/* If the user is logged in, user's id. FK into users. */
   userId VARCHAR(32) NOT NULL,
 	/* Page id. FK into pages. */
   pageId VARCHAR(32) NOT NULL,
