a62cecb9216b00fb75d5b9add5053915844c48c1
Jonathan Weissman <JGWeissman@gmail.com>
2016-02-21 11:57:44 -0800

    Use utf8mb4 to include 4 byte characters

--- schemas/votes.sql
+++ schemas/votes.sql
@@ -12,4 +12,4 @@ CREATE TABLE votes (
   /* Date this like was created. */
   createdAt DATETIME NOT NULL,
   PRIMARY KEY(id)
-) CHARACTER SETmb4 utf8 COLLATE utf8mb4_general_ci;
+) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
