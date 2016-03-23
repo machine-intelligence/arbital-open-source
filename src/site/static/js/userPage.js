5bfccb2e2b490a5b8cdd100d1592eda92bd9bf77
Nathan Rosquist <nathan.rosquist@kolkin.com>
2016-03-23 13:23:08 -0700

    Bring back userPage.html and userPage.js

---
+++ src/site/static/js/userPage.js
@@ -0,0 +1,16 @@
+"use strict";
+
+// Directive for the User page.
+app.directive("arbUserPage", function(pageService, userService) {
+	return {
+		templateUrl: "static/html/userPage.html",
+		scope: {
+			userId: "@",
+			idsMap: "=",
+		},
+		controller: function($scope) {
+			$scope.pageService = pageService;
+			$scope.userService = userService;
+		},
+	};
+});
