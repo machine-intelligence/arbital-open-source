58ec961f001d280f74d70940c302da3f461c78ea
Alexei Andreev <alexei.andreev@gmail.com>
2016-05-04 10:21:09 -0700

    Fixed changes.sql

--- schemas/helpers/changes.sql
+++ schemas/helpers/changes.sql
@@ -43,25 +43,6 @@ alter table likes drop column pageId;
 
 alter table subscriptions drop column userTrustSnapshotId;
 
-drop table invites;
-CREATE TABLE invites (
-	/* Id of user sending invite. FK into users. */
-	fromUserId VARCHAR(32) NOT NULL,
-	/* Id of domain that this invite is for. FK into pageInfos. */
-	domainId VARCHAR(32) NOT NULL,
-	/* Email address to send invite to. */
-	toEmail VARCHAR(100) NOT NULL,
-	/* Date this invite was created. */
-	createdAt DATETIME NOT NULL,
-	/* If a user claimed this invite, this is their id. FK into users. */
-	toUserId VARCHAR(32) NOT NULL,
-	/* Date this invite was claimed */
-	claimedAt DATETIME NOT NULL,
-	/* When the invite email was sent. */
-	emailSentAt DATETIME NOT NULL,
-
-	PRIMARY KEY(fromUserId,domainId,toEmail)
-) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
 DROP TABLE inviteEmailPairs;
 drop table userDomainBonusTrust;
 
@@ -78,3 +59,4 @@ alter table visits add column sessionId VARCHAR(32) NOT NULL after userId;
 alter table visits add column ipAddress VARCHAR(64) NOT NULL after sessionId;
 delete from groupMembers where userId=groupId;
 insert into groupMembers (userId,createdAt,groupId) select id,createdAt,id from users;
+alter table users drop column  inviteCode;
