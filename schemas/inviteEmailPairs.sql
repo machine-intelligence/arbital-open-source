0433880e0a4ce3de2e58cb5b186cbac43d89b7cc
ESRogs <esrogs@gmail.com>
2016-05-03 17:37:24 -0700

    remove unnecessary schema file

--- schemas/inviteEmailPairs.sql
+++
@@ -1,6 +0,0 @@
-/* An entry for every invite + email combination code. */
-CREATE TABLE inviteEmailPairs (
-	/* Invite's unique code. FK into invites */
-	code VARCHAR(32) NOT NULL,
-	PRIMARY KEY(code, email)
-) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
