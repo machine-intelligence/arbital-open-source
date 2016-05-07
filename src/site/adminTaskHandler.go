3f492e5a923ff1cb2d947930dd2e8a132cc03274
Alexei Andreev <alexei.andreev@gmail.com>
2016-05-07 15:05:24 -0700

    New admin task for sending invites

--- src/site/adminTaskHandler.go
+++ src/site/adminTaskHandler.go
@@ -16,31 +16,40 @@ var adminTaskHandler = siteHandler{
 
 // adminTaskHandlerFunc kicks off the task.
 func adminTaskHandlerFunc(params *pages.HandlerParams) *pages.Result {
+	c := params.C
 	task := params.R.FormValue("task")
 	if task == "fixText" {
 		var task tasks.FixTextTask
-		if err := tasks.Enqueue(params.C, &task, nil); err != nil {
+		if err := tasks.Enqueue(c, &task, nil); err != nil {
 			return pages.HandlerErrorFail("Couldn't enqueue a task", err)
 		}
 	} else if task == "populateElastic" {
 		var task tasks.PopulateElasticTask
-		if err := tasks.Enqueue(params.C, &task, nil); err != nil {
+		if err := tasks.Enqueue(c, &task, nil); err != nil {
 			return pages.HandlerErrorFail("Couldn't enqueue a task", err)
 		}
 	} else if task == "updateMetadata" {
 		var task tasks.UpdateMetadataTask
-		if err := tasks.Enqueue(params.C, &task, nil); err != nil {
+		if err := tasks.Enqueue(c, &task, nil); err != nil {
 			return pages.HandlerErrorFail("Couldn't enqueue a task", err)
 		}
 	} else if task == "propagateDomain" {
 		var task tasks.PropagateDomainTask
 		task.PageId = params.R.FormValue("pageId")
-		if err := tasks.Enqueue(params.C, &task, nil); err != nil {
+		if err := tasks.Enqueue(c, &task, nil); err != nil {
 			return pages.HandlerErrorFail("Couldn't enqueue a task", err)
 		}
 	} else if task == "resetPasswords" {
 		var task tasks.ResetPasswordsTask
-		if err := tasks.Enqueue(params.C, &task, nil); err != nil {
+		if err := tasks.Enqueue(c, &task, nil); err != nil {
+			return pages.HandlerErrorFail("Couldn't enqueue a task", err)
+		}
+	} else if task == "sendInvite" {
+		var task tasks.SendInviteTask
+		task.FromUserId = "1"
+		task.ToEmail = "alexei.andreev@gmail.com"
+		task.DomainIds = []string{"1lw", "2v", "3d"}
+		if err := tasks.Enqueue(c, &task, nil); err != nil {
 			return pages.HandlerErrorFail("Couldn't enqueue a task", err)
 		}
 	} else {
