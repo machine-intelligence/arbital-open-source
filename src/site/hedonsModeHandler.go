7b0a591144429415c89b8757b2b113350ecd0bc5
Stephanie Watson Zolayvar <zolayvars@gmail.com>
2016-05-18 13:03:41 -0700

    Show likes on your changes

--- src/site/hedonsModeHandler.go
+++ src/site/hedonsModeHandler.go
@@ -22,8 +22,9 @@ const (
 type HedonsRow struct {
 	Type            string          `json:"type"`
 	NewActivityAt   string          `json:"newActivityAt"`
-	UserIdsMap      map[string]bool `json:"userIdsMap"` // Unique userIds
+	UserIdsMap      map[string]bool `json:"userIdsMap"` // Unique userIds.
 	PageId          string          `json:"pageId"`
+	ChangeLog       *core.ChangeLog `json:"changeLog"`       // Optional changeLog.
 	RequisiteIdsMap map[string]bool `json:"requisiteIdsMap"` // Unique requisiteIds. Only present if type == ReqsTaughtType
 }
 
@@ -47,19 +48,30 @@ func hedonsModeHandlerFunc(params *pages.HandlerParams) *pages.Result {
 		return pages.Fail("Couldn't decode request", err).Status(http.StatusBadRequest)
 	}
 
+	hedons := make([]*HedonsRow, 0)
+
 	// Load new likes on my pages and comments
-	likesRows, err := loadReceivedLikes(db, u, returnData.PageMap, returnData.UserMap)
+	pageLikesRows, err := loadPageLikes(db, u, returnData.PageMap, returnData.UserMap)
 	if err != nil {
 		return pages.Fail("Error loading new likes", err)
 	}
+	hedons = append(hedons, pageLikesRows...)
+
+	// Load new likes on my changeLogs
+	changeLikesRows, err := loadChangeLikes(db, u, returnData.PageMap, returnData.UserMap)
+	if err != nil {
+		return pages.Fail("Error loading new likes", err)
+	}
+	hedons = append(hedons, changeLikesRows...)
 
 	// Load requisites taught
 	reqsTaughtRows, err := loadRequisitesTaught(db, u, returnData.PageMap, returnData.UserMap)
 	if err != nil {
 		return pages.Fail("Error loading requisites taught", err)
 	}
+	hedons = append(hedons, reqsTaughtRows...)
 
-	returnData.ResultMap["hedons"] = append(likesRows, reqsTaughtRows...)
+	returnData.ResultMap["hedons"] = hedons
 
 	// Load and update lastAchievementsView for this user
 	returnData.ResultMap[LastAchievementsModeView], err = LoadAndUpdateLastView(db, u, LastAchievementsModeView)
@@ -76,24 +88,21 @@ func hedonsModeHandlerFunc(params *pages.HandlerParams) *pages.Result {
 	return pages.Success(returnData)
 }
 
-func loadReceivedLikes(db *database.DB, u *core.CurrentUser, pageMap map[string]*core.Page, userMap map[string]*core.User) ([]*HedonsRow, error) {
+func loadPageLikes(db *database.DB, u *core.CurrentUser, pageMap map[string]*core.Page, userMap map[string]*core.User) ([]*HedonsRow, error) {
 	hedonsRowMap := make(map[string]*HedonsRow, 0)
 
 	rows := database.NewQuery(`
-		SELECT u.Id,pi.pageId,pi.type,l.updatedAt,l.value
+		SELECT l.userId,pi.pageId,pi.type,l.updatedAt
 		FROM `).AddPart(core.PageInfosTable(u)).Add(` AS pi
 		JOIN likes AS l
 		ON pi.likeableId=l.likeableId
-		JOIN users AS u
-		ON l.userId=u.id
 		WHERE pi.createdBy=?`, u.Id).Add(`
 			AND l.value=1 AND l.userId!=?`, u.Id).Add(`
 		ORDER BY l.updatedAt DESC`).ToStatement(db).Query()
 	err := rows.Process(func(db *database.DB, rows *database.Rows) error {
 		var likerId, pageId, pageType, updatedAt string
-		var likeValue int
 
-		err := rows.Scan(&likerId, &pageId, &pageType, &updatedAt, &likeValue)
+		err := rows.Scan(&likerId, &pageId, &pageType, &updatedAt)
 		if err != nil {
 			return fmt.Errorf("Failed to scan: %v", err)
 		}
@@ -107,14 +116,63 @@ func loadReceivedLikes(db *database.DB, u *core.CurrentUser, pageMap map[string]
 				UserIdsMap:    make(map[string]bool),
 			}
 			hedonsRowMap[pageId] = hedonsRow
-
-			loadOptions := core.TitlePlusLoadOptions
-			if pageType == core.CommentPageType {
-				loadOptions.Add(&core.PageLoadOptions{Parents: true})
-			}
-			core.AddPageToMap(pageId, pageMap, loadOptions)
 		}
 
+		loadOptions := core.TitlePlusLoadOptions
+		if pageType == core.CommentPageType {
+			loadOptions.Add(&core.PageLoadOptions{Parents: true})
+		}
+		core.AddPageToMap(pageId, pageMap, loadOptions)
+		core.AddUserIdToMap(likerId, userMap)
+		hedonsRow.UserIdsMap[likerId] = true
+		return nil
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	hedonsRows := make([]*HedonsRow, 0, len(hedonsRowMap))
+	for _, row := range hedonsRowMap {
+		hedonsRows = append(hedonsRows, row)
+	}
+
+	return hedonsRows, nil
+}
+
+func loadChangeLikes(db *database.DB, u *core.CurrentUser, pageMap map[string]*core.Page, userMap map[string]*core.User) ([]*HedonsRow, error) {
+	hedonsRowMap := make(map[int]*HedonsRow, 0)
+
+	rows := database.NewQuery(`
+		SELECT l.userId,cl.pageId,l.updatedAt,cl.id,cl.type,cl.oldSettingsValue,cl.newSettingsValue,cl.edit
+		FROM likes as l
+		JOIN changeLogs as cl
+		ON cl.likeableId=l.likeableId
+		WHERE cl.userId=?`, u.Id).Add(`
+			AND l.value=1 AND l.userId!=?`, u.Id).Add(`
+		ORDER BY l.updatedAt DESC`).ToStatement(db).Query()
+	err := rows.Process(func(db *database.DB, rows *database.Rows) error {
+		var likerId, pageId, updatedAt string
+		var changeLog core.ChangeLog
+
+		err := rows.Scan(&likerId, &pageId, &updatedAt,
+			&changeLog.Id, &changeLog.Type, &changeLog.OldSettingsValue, &changeLog.NewSettingsValue, &changeLog.Edit)
+		if err != nil {
+			return fmt.Errorf("Failed to scan: %v", err)
+		}
+
+		hedonsRow, ok := hedonsRowMap[changeLog.Id]
+		if !ok {
+			hedonsRow = &HedonsRow{
+				PageId:        pageId,
+				NewActivityAt: updatedAt,
+				Type:          LikesRowType,
+				ChangeLog:     &changeLog,
+				UserIdsMap:    make(map[string]bool),
+			}
+			hedonsRowMap[changeLog.Id] = hedonsRow
+		}
+
+		core.AddPageToMap(pageId, pageMap, core.TitlePlusLoadOptions)
 		core.AddUserIdToMap(likerId, userMap)
 		hedonsRow.UserIdsMap[likerId] = true
 		return nil
@@ -136,12 +194,10 @@ func loadRequisitesTaught(db *database.DB, u *core.CurrentUser, pageMap map[stri
 	hedonsRowMap := make(map[string]*HedonsRow, 0)
 
 	rows := database.NewQuery(`
-		SELECT u.Id,pi.pageId,ump.masteryId,ump.updatedAt
+		SELECT ump.userId,pi.pageId,ump.masteryId,ump.updatedAt
 		FROM userMasteryPairs AS ump
 		JOIN `).AddPart(core.PageInfosTable(u)).Add(` AS pi
 		ON ump.taughtBy=pi.pageId
-		JOIN users AS u
-		ON ump.userId=u.id
 		WHERE pi.createdBy=?`, u.Id).Add(`
 			AND ump.has=1 AND ump.userId!=?`, u.Id).Add(`
 		ORDER BY ump.updatedAt DESC`).ToStatement(db).Query()
