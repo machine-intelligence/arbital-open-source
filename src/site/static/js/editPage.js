b1b766c4abba8157752a12cfddccf20340e1eaa4
Alexei Andreev <alexei.andreev@gmail.com>
2016-06-09 22:08:15 -0700

    Quick fix for interrupting save

--- src/site/static/js/editPage.js
+++ src/site/static/js/editPage.js
@@ -277,12 +277,12 @@ app.directive('arbEditPage', function($location, $filter, $timeout, $interval, $
 			$timeout(function() {
 				// If we loaded an edit that's not based off of the current edit, freeze!
 				// Note: do this before we compute prevEditPageData, to make sure autosave goes through.
-				if ($scope.page.wasPublished && $scope.page.prevEdit != arb.stateService.getPage($scope.pageId).currentEdit) {
+				/*if ($scope.page.wasPublished && $scope.page.prevEdit != arb.stateService.getPage($scope.pageId).currentEdit) {
 					$scope.freezeEdit = true;
 					$scope.savePage(false, true);
 					var message = 'A new version was published. To prevent edit conflicts, please refresh the page to see it. (A snapshot of your current state has been saved.)';
 					$scope.addMessage('obsoleteEdit', message, 'error');
-				}
+				}*/
 				// Compute prevEditPageData, so we don't fire off autosave when there were
 				// no changes made.
 				prevEditPageData = computeAutosaveData();
