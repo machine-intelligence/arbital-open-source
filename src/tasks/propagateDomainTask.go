2234ec21ecaa556c972a27e6ac3732fd33b38b51
ESRogs <esrogs@gmail.com>
2016-06-21 17:48:57 -0700

    move _propagateDomainsToSinglePage() into its own function

--- src/tasks/propagateDomainTask.go
+++ src/tasks/propagateDomainTask.go
@@ -47,7 +47,7 @@ func (task PropagateDomainTask) Execute(db *database.DB) (delay int, err error) 
 	// Process the first page.
 	// Map of pageId -> whether or not we processed the children
 	pageMap := make(map[string]bool)
-	err = propagateDomainToPage(db, task.PageId, pageMap)
+	err = propagateDomainsToPageAndDescendants(db, task.PageId, pageMap)
 	if err != nil {
 		return -1, fmt.Errorf("Error propagating domain: %v", err)
 	}
@@ -55,95 +55,105 @@ func (task PropagateDomainTask) Execute(db *database.DB) (delay int, err error) 
 	return 0, nil
 }
 
-// propagateDomainToPage forces domain recalculation for the given page.
+func _propagateDomainsToSinglePage(db *database.DB, pageId string) ([]interface{}, []interface{}, error) {
+	// Compute what domains the page already has
+	domainMap := make(map[string]*domainFlags)
+	rows := db.NewStatement(`
+		SELECT domainId
+		FROM pageDomainPairs
+		WHERE pageId=?`).Query(pageId)
+	err := rows.Process(func(db *database.DB, rows *database.Rows) error {
+		var domainId string
+		if err := rows.Scan(&domainId); err != nil {
+			return fmt.Errorf("failed to scan for pageDomainPair: %v", err)
+		}
+		domainMap[domainId] = &domainFlags{Has: true}
+		return nil
+	})
+	if err != nil {
+		return nil, nil, fmt.Errorf("Faled to load pageDomainPairs: %v", err)
+	}
+
+	// Compute what domains the page should have based on its parents and
+	// whether or not it's a root page for some domain
+	rows = database.NewQuery(`
+		(
+			/* get the domains (and see-groups) of parents */
+			SELECT pdp.domainId, pi.seeGroupId
+			FROM (
+				SELECT parentId AS pageId
+				FROM pagePairs AS pp
+				WHERE pp.childId=?`, pageId).Add(`AND pp.type=?`, core.ParentPagePairType).Add(`
+			) AS parents
+			JOIN pageDomainPairs AS pdp ON pdp.pageId=parents.pageId
+			JOIN`).AddPart(core.PageInfosTable(nil)).Add(`AS pi ON pi.pageId=parents.pageId
+		) UNION (
+			/* include the given page, if it's the root of a domain */
+			SELECT pi.pageId, pi.seeGroupId
+			FROM`).AddPart(core.PageInfosTable(nil)).Add(`AS pi
+			WHERE pi.pageId=?`, pageId).Add(`AND pi.type=?`, core.DomainPageType).Add(`
+		)`).ToStatement(db).Query()
+	err = rows.Process(func(db *database.DB, rows *database.Rows) error {
+		var domainId, seeGroupId string
+		if err := rows.Scan(&domainId, &seeGroupId); err != nil {
+			return fmt.Errorf("failed to scan for pageDomainPair: %v", err)
+		}
+		if flags, ok := domainMap[domainId]; ok {
+			flags.ShouldHave = !core.IsIdValid(seeGroupId)
+		} else {
+			domainMap[domainId] = &domainFlags{ShouldHave: seeGroupId == ""}
+		}
+		return nil
+	})
+	if err != nil {
+		return nil, nil, fmt.Errorf("Faled to load pageDomainPairs2: %v", err)
+	}
+
+	// Compute which domains to add/remove
+	addDomainArgs := make([]interface{}, 0)
+	removeDomainArgs := make([]interface{}, 0)
+	for domainId, flags := range domainMap {
+		if flags.ShouldHave && !flags.Has {
+			addDomainArgs = append(addDomainArgs, domainId, pageId)
+		} else if !flags.ShouldHave && flags.Has {
+			removeDomainArgs = append(removeDomainArgs, domainId)
+		}
+	}
+
+	// Add missing domains
+	if len(addDomainArgs) > 0 {
+		statement := db.NewStatement(`
+			INSERT INTO pageDomainPairs
+			(domainId,pageId) VALUES ` + database.ArgsPlaceholder(len(addDomainArgs), 2))
+		if _, err = statement.Exec(addDomainArgs...); err != nil {
+			return nil, nil, fmt.Errorf("Failed to add to pageDomainPairs: %v", err)
+		}
+	}
+
+	// Remove obsolete domains
+	if len(removeDomainArgs) > 0 {
+		statement := db.NewStatement(`
+			DELETE FROM pageDomainPairs
+			WHERE pageId=? AND domainId IN ` + database.InArgsPlaceholder(len(removeDomainArgs)))
+		args := append([]interface{}{pageId}, removeDomainArgs...)
+		if _, err = statement.Exec(args...); err != nil {
+			return nil, nil, fmt.Errorf("Failed to remove pageDomainPairs: %v", err)
+		}
+	}
+
+	return addDomainArgs, removeDomainArgs, nil
+}
+
+// propagateDomainsToPageAndDescendants forces domain recalculation for the given page and its children.
 // It uses a depth-first search to process the children of the page.
-func propagateDomainToPage(db *database.DB, pageId string, pageMap map[string]bool) error {
+func propagateDomainsToPageAndDescendants(db *database.DB, pageId string, pageMap map[string]bool) error {
 	db.C.Infof("Processing %s", pageId)
 	processedChildren, processedPage := pageMap[pageId]
 	if !processedPage {
-		// Compute what domains the page already has
-		domainMap := make(map[string]*domainFlags)
-		rows := db.NewStatement(`
-			SELECT domainId
-			FROM pageDomainPairs
-			WHERE pageId=?`).Query(pageId)
-		err := rows.Process(func(db *database.DB, rows *database.Rows) error {
-			var domainId string
-			if err := rows.Scan(&domainId); err != nil {
-				return fmt.Errorf("failed to scan for pageDomainPair: %v", err)
-			}
-			domainMap[domainId] = &domainFlags{Has: true}
-			return nil
-		})
+
+		addDomainArgs, removeDomainArgs, err := _propagateDomainsToSinglePage(db, pageId)
 		if err != nil {
-			return fmt.Errorf("Faled to load pageDomainPairs: %v", err)
-		}
-
-		// Compute what domains the page should have based on its parents and
-		// whether or not it's a root page for some domain
-		rows = database.NewQuery(`
-			(
-				/* get the domains (and see-groups) of parents */
-				SELECT pdp.domainId, pi.seeGroupId
-				FROM (
-					SELECT parentId AS pageId
-					FROM pagePairs AS pp
-					WHERE pp.childId=?`, pageId).Add(`AND pp.type=?`, core.ParentPagePairType).Add(`
-				) AS parents
-				JOIN pageDomainPairs AS pdp ON pdp.pageId=parents.pageId
-				JOIN`).AddPart(core.PageInfosTable(nil)).Add(`AS pi ON pi.pageId=parents.pageId
-			) UNION (
-				/* include the given page, if it's the root of a domain */
-				SELECT pi.pageId, pi.seeGroupId
-				FROM`).AddPart(core.PageInfosTable(nil)).Add(`AS pi
-				WHERE pi.pageId=?`, pageId).Add(`AND pi.type=?`, core.DomainPageType).Add(`
-			)`).ToStatement(db).Query()
-		err = rows.Process(func(db *database.DB, rows *database.Rows) error {
-			var domainId, seeGroupId string
-			if err := rows.Scan(&domainId, &seeGroupId); err != nil {
-				return fmt.Errorf("failed to scan for pageDomainPair: %v", err)
-			}
-			if flags, ok := domainMap[domainId]; ok {
-				flags.ShouldHave = !core.IsIdValid(seeGroupId)
-			} else {
-				domainMap[domainId] = &domainFlags{ShouldHave: seeGroupId == ""}
-			}
-			return nil
-		})
-		if err != nil {
-			return fmt.Errorf("Faled to load pageDomainPairs2: %v", err)
-		}
-
-		// Compute which domains to add/remove
-		addDomainArgs := make([]interface{}, 0)
-		removeDomainArgs := make([]interface{}, 0)
-		for domainId, flags := range domainMap {
-			if flags.ShouldHave && !flags.Has {
-				addDomainArgs = append(addDomainArgs, domainId, pageId)
-			} else if !flags.ShouldHave && flags.Has {
-				removeDomainArgs = append(removeDomainArgs, domainId)
-			}
-		}
-
-		// Add missing domains
-		if len(addDomainArgs) > 0 {
-			statement := db.NewStatement(`
-				INSERT INTO pageDomainPairs
-				(domainId,pageId) VALUES ` + database.ArgsPlaceholder(len(addDomainArgs), 2))
-			if _, err = statement.Exec(addDomainArgs...); err != nil {
-				return fmt.Errorf("Failed to add to pageDomainPairs: %v", err)
-			}
-		}
-
-		// Remove obsolete domains
-		if len(removeDomainArgs) > 0 {
-			statement := db.NewStatement(`
-				DELETE FROM pageDomainPairs
-				WHERE pageId=? AND domainId IN ` + database.InArgsPlaceholder(len(removeDomainArgs)))
-			args := append([]interface{}{pageId}, removeDomainArgs...)
-			if _, err = statement.Exec(args...); err != nil {
-				return fmt.Errorf("Failed to remove pageDomainPairs: %v", err)
-			}
+			return err
 		}
 
 		// Make the page as processed
@@ -162,7 +172,7 @@ func propagateDomainToPage(db *database.DB, pageId string, pageMap map[string]bo
 			if err := rows.Scan(&childId); err != nil {
 				return fmt.Errorf("failed to scan for childId: %v", err)
 			}
-			err := propagateDomainToPage(db, childId, pageMap)
+			err := propagateDomainsToPageAndDescendants(db, childId, pageMap)
 			return err
 		})
 		if err != nil {
