7b9f76c79a4fc11e46a22b5de6695b97a329bdae
Alexei Andreev <alexei.andreev@gmail.com>
2016-06-22 10:15:01 -0700

    Require a whitespace character before $...$ mathjax

--- src/site/static/js/markdownService.js
+++ src/site/static/js/markdownService.js
@@ -293,7 +293,7 @@ app.service('markdownService', function($compile, $timeout, pageService, userSer
 			});
 		});
 		// Process $mathjax$ spans.
-		var mathjaxSpanRegexp = new RegExp(notEscaped + '(~D' + getMathjaxRegexp(false) + '~D)', 'g');
+		var mathjaxSpanRegexp = new RegExp('(^|\\s)(~D' + getMathjaxRegexp(false) + '~D)', 'g');
 		converter.hooks.chain('preSpanGamut', function(text) {
 			return text.replace(mathjaxSpanRegexp, function(whole, prefix, mathjaxText) {
 				if (mathjaxText.substring(0, 4) == '~D~D') return whole;
