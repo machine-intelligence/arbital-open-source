baa9618a2a11d19ae94d6a0d90ace59e7ea58196
Stephanie Zolayvar <zolayvars@gmail.com>
2016-07-04 16:19:30 -0700

    Start of pageImprovement improvement

--- src/site/static/js/pageImprovement.js
+++ src/site/static/js/pageImprovement.js
@@ -10,6 +10,22 @@ app.directive('arbPageImprovement', function($timeout, $http, $compile, arb) {
 		controller: function($scope) {
 			$scope.arb = arb;
 
+			// Determine which style of bar to show
+			$scope.qualityTag = 'unassessed';
+			if ($scope.page.taggedAsIds.includes('72')) {
+				$scope.qualityTag = 'stub';
+			} else if ($scope.page.taggedAsIds.includes('3rk')) {
+				$scope.qualityTag = 'start';
+			} else if ($scope.page.taggedAsIds.includes('4y7')) {
+				$scope.qualityTag = 'c-class';
+			} else if ($scope.page.taggedAsIds.includes('4yd')) {
+				$scope.qualityTag = 'b-class';
+			} else if ($scope.page.taggedAsIds.includes('4yf')) {
+				$scope.qualityTag = 'a-class';
+			} else if ($scope.page.taggedAsIds.includes('4yl')) {
+				$scope.qualityTag = 'featured';
+			}
+
 			$scope.shouldShowTagsColumn = function() {
 				return $scope.page.improvementTagIds.length > 0;
 			};
@@ -17,7 +33,7 @@ app.directive('arbPageImprovement', function($timeout, $http, $compile, arb) {
 				return $scope.page.todos.length > 0;
 			};
 			$scope.shouldShowImprovements = function() {
-				return $scope.shouldShowTagsColumn() || $scope.shouldShowTodosColumn();
+				return !['b-class', 'a-class', 'featured'].includes($scope.qualityTag);
 			};
 
 			$scope.expanded = $scope.page.isSubscribedAsMaintainer;
