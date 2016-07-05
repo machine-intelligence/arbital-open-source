ac308ac8e74b36273f53038f6d4e1d5dd282e3e6
Alexei Andreev <alexei.andreev@gmail.com>
2016-07-05 09:38:16 -0700

    Can quickly add tags

--- src/site/static/js/page.js
+++ src/site/static/js/page.js
@@ -73,6 +73,12 @@ app.directive('arbPage', function($http, $location, $compile, $timeout, $interva
 				}
 			};
 
+			// Show the panel to add tags
+			$scope.isTagsPanelVisible = false;
+			$scope.showTagsPanel = function() {
+				$scope.isTagsPanelVisible = true;
+			};
+
 			// Toggle between show delete answer buttons
 			$scope.showDeleteAnswer = false;
 			$scope.toggleDeleteAnswers = function() {
