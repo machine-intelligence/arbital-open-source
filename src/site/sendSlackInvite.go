51a80aa001caa5a17efba18609dec038f2406b48
ESRogs <esrogs@gmail.com>
2016-07-07 16:58:18 -0700

    another minor tweak to add-user-to-slack message

--- src/site/sendSlackInvite.go
+++ src/site/sendSlackInvite.go
@@ -54,7 +54,7 @@ func sendSlackInviteHandlerFunc(params *pages.HandlerParams) *pages.Result {
 			Sender:  "alexei@arbital.com",
 			To:      []string{"trigger@recipe.ifttt.com"},
 			Subject: fmt.Sprintf("#slackbot", data.Email),
-			Body:    fmt.Sprintf("%s (id: %s) wants to join Slack.\nSomeone should invite them via https://arbital.slack.com/admin", data.Email, u.Id),
+			Body:    fmt.Sprintf("%s (id: %s) wants to join Slack. Someone should invite them via: https://arbital.slack.com/admin", data.Email, u.Id),
 		}
 
 		err = mail.Send(c, msg)
