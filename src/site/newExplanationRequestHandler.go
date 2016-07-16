701e76fa64654968d975bb62a7a8a50e9c870150
ESRogs <esrogs@gmail.com>
2016-07-15 20:28:29 -0700

    clean up code in newExplanationRequestJsonHandler.go

--- src/site/newExplanationRequestHandler.go
+++ src/site/newExplanationRequestHandler.go
@@ -1,4 +1,4 @@
-// explanationRequestJsonHandler.go handles explantion requests
+// newExplanationRequestJsonHandler.go handles explantion requests
 package site
 
 import (
@@ -42,7 +42,7 @@ func explanationRequestJsonHandler(params *pages.HandlerParams) *pages.Result {
 
 	// Add the request.
 	err2 := db.Transaction(func(tx *database.Tx) sessions.Error {
-		return addNewExplanationRequest(tx, u, data.PageId, data.Type)
+		return plusOneToExplanationRequest(tx, u, data.PageId, data.Type)
 	})
 	if err2 != nil {
 		return pages.FailWith(err2)
@@ -58,51 +58,48 @@ func explanationRequestJsonHandler(params *pages.HandlerParams) *pages.Result {
 }
 
 // Add a like to the explanation request for the given (page, type) pair.
-func addNewExplanationRequest(tx *database.Tx, u *core.CurrentUser, pageId string, erType string) sessions.Error {
-	// check to see if there's already an explanation request for this (page, type) pair
-	id, err := _lookupExplanationRequest(tx.DB, u, pageId, erType)
+func plusOneToExplanationRequest(tx *database.Tx, u *core.CurrentUser, pageId string, requestType string) sessions.Error {
+	// Check to see if there's already an explanation request for this (page, type) pair.
+	alreadyExists, id, err := _lookupExplanationRequest(tx.DB, u, pageId, requestType)
 	if err != nil {
 		return sessions.NewError("Error querying for an existing explanation request", err)
 	}
 
-	// if an explanation request doesn't exist, create a new one
-	if id == "" {
-		idInt64, serr := _createExplanationRequest(tx, u, pageId, erType)
+	// If an explanation request doesn't exist, create a new one.
+	if !alreadyExists {
+		idInt64, serr := _createExplanationRequest(tx, u, pageId, requestType)
 		if serr != nil {
 			return sessions.NewError("Couldn't create a new explanation request", serr)
 		}
 		id = strconv.FormatInt(idInt64, 10)
 	}
 
-	// finally, add a like for the request
+	// Finally, add a like for the request.
 	return addNewLike(tx, u, 0, id, core.ContentRequestLikeableType, 1)
 }
 
 // Find the id of the explanation request for the given (page, type) pair.
-func _lookupExplanationRequest(db *database.DB, u *core.CurrentUser, pageId string, erType string) (string, error) {
+func _lookupExplanationRequest(db *database.DB, u *core.CurrentUser, pageId string, requestType string) (bool, string, error) {
 	var id string
 
-	rows := database.NewQuery(`
+	row := database.NewQuery(`
 		SELECT id
 		FROM contentRequests AS er
-		WHERE er.pageId=? AND er.type=?`,
-		pageId, erType).ToStatement(db).Query()
-	err := rows.Process(func(db *database.DB, rows *database.Rows) error {
-		err := rows.Scan(&id)
-		if err != nil {
-			return fmt.Errorf("failed to scan an explanation request id: %v", err)
-		}
-		return nil
-	})
+		WHERE er.pageId=?`, pageId).Add(`
+			AND er.type=?`, requestType).ToStatement(db).QueryRow()
+	exists, err := row.Scan(&id)
+	if err != nil {
+		return false, "", fmt.Errorf("failed to scan an explanation request id: %v", err)
+	}
 
-	return id, err
+	return exists, id, nil
 }
 
 // Insert a new explanation request row into the table.
-func _createExplanationRequest(tx *database.Tx, u *core.CurrentUser, pageId string, erType string) (int64, error) {
+func _createExplanationRequest(tx *database.Tx, u *core.CurrentUser, pageId string, requestType string) (int64, error) {
 	hashmap := make(map[string]interface{})
 	hashmap["pageId"] = pageId
-	hashmap["type"] = erType
+	hashmap["type"] = requestType
 	hashmap["createdAt"] = database.Now()
 	statement := tx.DB.NewInsertStatement("contentRequests", hashmap)
 	result, err := statement.WithTx(tx).Exec()
