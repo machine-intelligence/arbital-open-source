63911ab0c64b13951c3e46af3993505c2ffcc4c1
Jeremy Schlatter <jeremy.schlatter@gmail.com>
2016-07-18 19:24:18 -0700

    Fix precommit hook
    - IFS=$'\n'
        This didn't work on my shell. It set IFS to "n" rather than the
        newline character.
    - >/dev/null
        This seems to have made the script not work at all. I'm confused
        about how it managed to get there. This commit removes it.
    - Add "-s" flag to error message.

--- scripts/needs_gofmt.sh
+++ scripts/needs_gofmt.sh
@@ -3,16 +3,17 @@
 # Checks if any files about to be committed need gofmt'ing.
 
 echo "Checking if any files need gofmt.." >&2
-IFS=$'\n'
+IFS="
+"
 if git rev-parse HEAD >/dev/null 2>&1; then
 		FILES=$(git diff --cached --name-only | grep -e '\.go$');
 else
 		FILES=$(git ls-files -c | grep -e '\.go$');
 fi
 for file in $FILES; do
-		badfile="$(git --no-pager show :"$file" >/dev/null 2>&1 | gofmt -s -l)"
+		badfile="$(git --no-pager show :"$file" | gofmt -s -l)"
 		if test -n "$badfile" ; then
-				echo "git pre-commit check failed: file needs gofmt: $file" >&2
+				echo "git pre-commit check failed: file needs 'gofmt -s': $file" >&2
 				exit 1
 		fi
 done
