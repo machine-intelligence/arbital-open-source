5b24ab1dc851e64d980ca662beb73bc1d2fc6734
Stephanie Zolayvar <zolayvars@gmail.com>
2016-08-16 17:21:10 -0700

    Some stuff

--- src/site/static/js/indexPage.ts
+++ src/site/static/js/indexPage.ts
@@ -40,6 +40,20 @@ app.directive('arbIndex', function($http, $mdMedia, arb) {
 			$scope.selectWriteTab = function(tab) {
 				$scope.writeTab = tab;
 			};
+
+			$scope.stubs = [
+				'We only care about things up to isomorphism',
+				'We can describe objects based entirely on how they interact with other objects',
+				'The category of finite sets',
+				'The empty set',
+				'Disjoint union of finite sets',
+				'Union of finite sets',
+				'The empty set can be described enitrely by its universal property',
+				'The union and product can be described entirely by their universal properties, up to isomorphism',
+				'Least upper bound and greatest lower bounds',
+				'The universal properties of the LUB and GLB',
+				'This kind of property crops up all over the place'
+			];
 		},
 	};
 });
