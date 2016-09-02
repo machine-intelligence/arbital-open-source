243a51c7ef204cae33486a675e4e33272dc24d65
Stephanie Zolayvar <zolayvars@gmail.com>
2016-09-02 10:18:42 -0700

    Change universal property project text

--- src/site/indexJsonHandler.go
+++ src/site/indexJsonHandler.go
@@ -21,6 +21,12 @@ func indexJSONHandler(params *pages.HandlerParams) *pages.Result {
 	// Load pages.
 	core.AddPageIDToMap("3hs", returnData.PageMap)
 	core.AddPageIDToMap("4ym", returnData.PageMap)
+	core.AddPageToMap("4c7", returnData.PageMap, core.TitlePlusLoadOptions)
+	core.AddPageToMap("600", returnData.PageMap, core.TitlePlusLoadOptions)
+	core.AddPageToMap("5xx", returnData.PageMap, core.TitlePlusLoadOptions)
+	core.AddPageToMap("1ln", returnData.PageMap, core.TitlePlusLoadOptions)
+	core.AddPageToMap("5zs", returnData.PageMap, core.TitlePlusLoadOptions)
+	core.AddPageToMap("3rb", returnData.PageMap, core.TitlePlusLoadOptions)
 	err := core.ExecuteLoadPipeline(db, returnData)
 	if err != nil {
 		return pages.Fail("Pipeline error", err)
