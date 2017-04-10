775872eac2e05e17ebd63ca1cee9cedb0feb2086
Stephanie Zolayvar <catorceovejas@gmail.com>
2017-04-09 21:05:56 -0700

    Also, add files for that last commit

---
+++ src/site/static/js/paragraphEditDialog.ts
@@ -0,0 +1,33 @@
+import app from './angular.ts';
+
+// EditPageDialogController is used for editing a page in an mdDialog
+app.controller('ParagraphEditDialogController', function($scope, $mdDialog, $timeout, arb, page, paragraphIndex) {
+	$scope.arb = arb;
+	$scope.page = page;
+	$scope.paragraphIndex = paragraphIndex;
+
+	// Load the page edit
+	arb.pageService.loadEdit({
+		pageAlias: page.pageId,
+		success: function() {
+			$scope.pageId = page.pageId;
+		},
+	});
+
+	// Called when the user is done editing the page
+	$scope.doneFn = function(result) {
+		$mdDialog.hide(result);
+	};
+
+	// Called when the user closed the dialog
+	$scope.hide = function() {
+		$mdDialog.hide({hidden: true, pageId: $scope.pageId});
+	};
+
+	$timeout(function() {
+		// We need this class in the beginning to make sure dialog appears in the
+		// correct position; but then we need to remove it, otherwise the body
+		// is not visible.
+		$('body').removeClass('md-dialog-is-showing');
+	});
+});
