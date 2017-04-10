654c0e8d69d49e6e8545f7972effaddc7af6ad54
Stephanie Zolayvar <catorceovejas@gmail.com>
2017-04-10 13:18:09 -0700

    forgot to add

---
+++ src/site/static/js/summaryEditDialogController.ts
@@ -0,0 +1,47 @@
+import app from './angular.ts';
+
+// EditPageDialogController is used for editing a page in an mdDialog
+app.controller('SummaryEditDialogController', function($scope, $mdDialog, $timeout, arb, page, summaryName, summaryText) {
+	$scope.arb = arb;
+	$scope.page = page;
+	$scope.summaryName = summaryName;
+	$scope.summaryText = summaryText;
+
+	// Load the page edit
+	arb.pageService.loadEdit({
+		pageAlias: page.pageId,
+		success: function() {
+			$scope.pageId = page.pageId;
+		},
+	});
+
+	$scope.maybeGetSummaryName = function() {
+		if (summaryName == 'Summary') {
+			return;
+		}
+		return summaryName; 
+	};
+
+	// Called when the user is done editing the page
+	$scope.doneFn = function(result) {
+		arb.pageService.loadEdit({
+			pageAlias: page.pageId,
+			success: function() {
+				$scope.pageId = page.pageId;
+			},
+		});
+		$mdDialog.hide(result);
+	};
+
+	// Called when the user closed the dialog
+	$scope.hide = function() {
+		$mdDialog.hide({hidden: true, pageId: $scope.pageId});
+	};
+
+	$timeout(function() {
+		// We need this class in the beginning to make sure dialog appears in the
+		// correct position; but then we need to remove it, otherwise the body
+		// is not visible.
+		$('body').removeClass('md-dialog-is-showing');
+	});
+});
